cef99fad2007e885ddfd85948a76d26c125e5e54
Ahmet YILMAZ <ahmeet.ylmzzz@gmail.com>
2025-10-14 17:39:02 +0300

    open_graph_image.png eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,22 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Ahmet Yılmaz | Flutter Developer",
   description: "Ahmet Yılmaz Portfolio",
+  openGraph: {
+    title: "Ahmet | Flutter Developer",
+    description: "Ahmet Portfolio",
+    url: "https://ahmetyilmazz.github.io/",
+    siteName: "Ahmet Portfolio",
+    images: [
+      {
+        url: "https://ahmetyilmazz.github.io/open_graph_image.png",
+        width: 1200,
+        height: 630,
+        alt: "Ahmet Portfolio",
+      },
+    ],
+    locale: "tr_TR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
